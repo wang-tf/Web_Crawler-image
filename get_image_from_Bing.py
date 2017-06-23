a7e80658e8f052388a36a2a0ea2f8d2e6ae4b850
WANG Tengfei <ternencewang@126.com>
2017-06-23 10:55:05 +0800

    Update get_image_from_Bing.py

--- get_image_from_Bing.py
+++ get_image_from_Bing.py
@@ -9,56 +9,98 @@
 ################################################
 
 import urllib
-import urllib2
-import re
-import datetime
 import os
 import time
 import requests
 from bs4 import BeautifulSoup
-import gzip
-import StringIO
+import sys
+import certifi
 
 
-def getManyPages(keyword, page):
+def makedir(localPath, keyword):
+    localPath = os.path.join(localPath, keyword)
+    if not os.path.exists(localPath):
+        os.mkdir(localPath)
+        
+#     localPath = os.path.join(localPath, time.strftime("BING:%Y-%m-%d %H:%M", time.localtime(time.time())))
+#     if not os.path.exists(localPath):
+#         os.mkdir(localPath)
+    if os.path.exists(localPath + '/URLlist.txt'):
+        os.remove(localPath + '/URLlist.txt') 
+        
+    return localPath
+
+def getImgUrl(keyword, page):
+    
     links = []
     keyword = urllib.pathname2url(keyword)
 
-    for i in range(page):
-        pn = i * 35
-        print("i=%s pn=%s" % (i, pn))
-        url = 'http://cn.bing.com/images/async?q=%s&first=%s&count=35&relp=35&lostate=r&mmasync=1&dgState=x*152_y*1187_h*190_c*1_i*141_r*27&IG=F7F9D77717C14FBBB80ACC9F0099C872&SFX=5&iid=images.5651' % (
-        keyword, pn)
-        print(url)
-        page = urllib.urlopen(url)
-        html = page.read()
-        soup = BeautifulSoup(html, 'lxml')
-        allmimgImages = soup.findAll(attrs={'class':"iusc"})
-        imgurl = soup.select(".iusc")
-        for i, image in enumerate(imgurl):
-            link = eval(image.attrs['m'])['murl']
-            links.append(link)
+    pn = page * 35
+    url = 'http://cn.bing.com/images/async?q=%s&first=%s&count=35&relp=35&lostate=r&mmasync=1&dgState=x*152_y*1187_h*190_c*1_i*141_r*27&IG=F7F9D77717C14FBBB80ACC9F0099C872&SFX=5&iid=images.5651' % (
+    keyword, pn)
+    html = urllib.urlopen(url).read()
+    soup = BeautifulSoup(html, 'lxml')
+    allmimgImages = soup.findAll(attrs={'class':"iusc"})
+    imgurl = soup.select(".iusc")
+    for i, image in enumerate(imgurl):
+        link = eval(image.attrs['m'])['murl']
+        links.append(link)
     return(links)
 
-def getImg(dataList, localPath):
-    localPaht = os.path.join(localPath, time.strftime("%Y-%m-%d %H:%M", time.localtime(time.time())))
-    if not os.path.exists(localPath):
-        os.mkdir(localPath)
-    if os.path.exists(localPath + '/URLlist.txt'):
-        os.remove(localPath + '/URLlist.txt')
+def downImg(imgURL, dirPath, name):
+
+    with open(dirPath + '/URLlist.txt', 'a') as f:
+        f.write(imgURL + '\n')
+    try:
+        ir = requests.get(imgURL, verify=certifi.old_where(), timeout=15)
+#         if str(ir.status_code)[0] == '4':
+#             print("\n%s:%s" % (str(ir.status_code), imgURL))
+#             return False
+    except Exception as e:
+        print("\nERROR:%s" % imgURL)
+        print(e)
+        return False
+    imgtype = imgURL.split('.')[-1]
+    if imgtype.find('/'):
+        imgtype = 'jpg'
+    with open((dirPath + '/%d.' + imgtype) % name, 'wb') as f:
+        f.write(ir.content)
+        return True
+
+def getKeyword(keywordsPath):
+    keywordlist = []
+    with open(keywordsPath, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            keywordlist.append(line.split('\n')[0])
+        return(keywordlist)    
     
-    x = 0
-    for imgURL in dataList:
-        print("正在下载imgURL:%s" % imgURL)
-        open(localPath + '/URLlist.txt', 'a').write(imgURL + '\n')
-        try:
-            ir = requests.get(imgURL, verify = False)
-        except :
-            print("ERROR")
-        open(localPath + '/bing-%d.jpg' % x, 'wb').write(ir.content)
-        x += 1
-    print(u'下载结束')
+def main(keyword):
+
+    localPath = '/image_dir/' # 下载图片保存的位置
+    pages = 500
+    x = 1
+    allofurl = 0
+    
+    savePath = makedir(localPath, keyword)
+    print(savePath)
+    for i in range(pages):
+        dataList = getImgUrl(keyword, i)
+        for imgURL in dataList:
+            allofurl += 1
+            if downImg(imgURL, savePath, x):
+                sys.stdout.write('\r成功下载\t%s/%s张' % (x, allofurl))
+                sys.stdout.flush()
+                x += 1
+            if x > 200:
+                break
+        if x > 200:
+            break
+
+    print(u'\n下载结束,成功 %s 个, 失败 %s 个' % (x-1, pages*35-x+1))    
 
 if __name__ == '__main__':
-    dataList = getManyPages('动漫', 4)
-    getImg(dataList, 'data0/users/wangtengfei/pictures/')
+    keywords_path = '/state_leaders_temp_bing.txt' # 包含搜索关键词的文本文件
+    keywordslist = getKeyword(keywords_path)
+    for keyword in  keywordslist:
+        main(keyword)
