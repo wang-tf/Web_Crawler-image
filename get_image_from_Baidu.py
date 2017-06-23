c2fdd48dcfb3aa3e13e94e5280c7f7812901e65e
WANG Tengfei <ternencewang@126.com>
2017-06-23 10:52:04 +0800

    Update get_image_from_Baidu.py

--- get_image_from_Baidu.py
+++ get_image_from_Baidu.py
@@ -2,8 +2,8 @@
 #_*_ coding:utf-8 _*_
 ################################################
 #
-# file: get_image_1.py
-# authou: shixi_tengfei5
+# file: get_image_from_baidu.py
+# authou: wang-tf
 # date: 2017-06-07 09:31
 #
 ################################################
@@ -12,6 +12,8 @@ import requests
 import os
 import time
 import sys
+import certifi
+import re
 
 maplist = {
         'w': 'a',
@@ -48,35 +50,47 @@ maplist = {
         'l': '9',
         'a': '0'
 }
+str_maplist = {
+    '_z2C$q': ':',
+    '_z&e3B': '.',
+    'AzdH3F': '/'
+}
+
+char_table = {ord(key): ord(value) for key, value in maplist.items()}
+
+def makedir(local_path, keyword):
+    
+    local_path = os.path.join(local_path, keyword)
+    if not os.path.exists(local_path):
+        os.mkdir(local_path)
+    # 增加日期文件夹
+#     local_path = os.path.join(local_path, time.strftime("BAIDU:%Y-%m-%d %H:%M", time.localtime(time.time())))
+#     if not os.path.exists(local_path):
+#         os.mkdir(local_path)
+    # 创建链接保存文件
+    if os.path.exists(local_path + '/URLlist.txt'):
+        os.remove(local_path + '/URLlist.txt')     
+    
+    return local_path
 
 def uncompile(url):
-    start = 0
-    end = 1
-    outurl = []
-    output = url.replace('_z2C$q', ':')
-    output = output.replace('_z&e3B', '.')
-    output = output.replace('AzdH3F', '/')
-    
-    url = output
-    while end <= len(url):
-        change = False
-        for key in maplist:
-            if url[start:end] == key:
-                outurl.append(maplist[key])
-                start = end
-                end = start + 1
-                change = True
-                break
-        if change == False:
-            outurl.append(url[start:end])
-            start = end
-            end = start + 1
-    return(''.join(outurl))
 
-def getManyPages(keyword,pages):
-    params=[]
-    for i in range(0,30*pages,30):
-        params.append({
+    for key, value in str_maplist.items():
+        url = url.replace(key, value)
+    return url.translate(char_table)             
+                
+def getKeyword(keywordsPath):
+    keywordlist = []
+    with open(keywordsPath, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            keywordlist.append(line.replace('\n','').strip())
+        return(keywordlist)
+    
+re_url = re.compile(r'"objURL":"(.*?)"')
+def getUrlFromPage(keyword, page_number):
+    
+    params = {
                       'tn': 'resultjson_com',
                       'ipn': 'rj',
                       'ct': 201326592,
@@ -102,59 +116,66 @@ def getManyPages(keyword,pages):
                       'qc': '',
                       'nc': 1,
                       'fr': '',
-                      'pn': i,
+                      'pn': page_number * 30,
                       'rn': 30,
                       'gsm': '1e',
                       '1488942260214': ''
-                  })
+                  }
     url = 'https://image.baidu.com/search/acjson'
-    urls = []
-    for i in params:
-        urls.append(requests.get(url,params=i,verify=False).json().get('data'))
-
+    urlcontent = requests.get(url, params=params, verify=certifi.old_where()).content.decode('utf-8')    
+    urls = [uncompile(x) for x in re_url.findall(urlcontent)]
     return urls
 
+def downloadUrl(img_url,save_path, save_name):
+    try:
+        ir = requests.get(img_url, timeout=15)
+#         if str(ir.status_code)[0] == '4':
+#             print("\n%s:%s" % (str(ir.status_code), imgURL))
+#             return False
+    except Exception as e:
+        print("\nERRORURL:%s" % img_url)
+        print(e)
+        return False
+    
+    img_type = img_url.split('.')[-1]
+    if img_type.find('/'):
+        img_type = 'jpg'
+    
+    with open((save_path + '/%s.' + img_type) % str(save_name), 'wb') as f:
+        f.write(ir.content)
+        return True    
 
-def getImg(dataList, localPath):
-
-    localPath = os.path.join(localPath, time.strftime("BAIDU:%Y-%m-%d %H:%M", time.localtime(time.time())))
-    if not os.path.exists(localPath):
-        os.mkdir(localPath)
-    if os.path.exists(localPath + '/URLlist.txt'):
-        os.remove(localPath + '/URLlist.txt')            
-
-    x = 1
-    error_num = 0
-    for list in dataList:
-        for i in list:
-            if i == {}:
-                continue
-            if i.get('objURL') != None:
-                imgURL = uncompile(i.get('objURL'))
-                sys.stdout.write('\r正在下载第\t%s个' % x)
+def main(keyword, local_path):
+    # pages为要下载的网页数，每页30张图片
+    pages = 500
+    # 按序命名下载的图片，并统计
+    img_number = 1
+    url_number = 0
+    # 创建对应的文件夹
+    save_path = makedir(local_path, keyword)
+    print(save_path)
+    # 抓取url并下载图片
+    for page_number in range(pages):
+        url_list = getUrlFromPage(keyword, page_number)
+        for url in url_list:
+            url_number += 1
+            if downloadUrl(url, save_path, img_number):
+                sys.stdout.write('\r成功下载\t%s/%s张' % (img_number, url_number))
                 sys.stdout.flush()
-                open(localPath + '/URLlist.txt', 'a').write(imgURL + '\n')
-                try:
-                    ir = requests.get(imgURL, verify=False)
-                except requests.ConnectionError:
-                    error_num += 1
-                    print("\nERRORURL:%s" % imgURL)
-                    print(ir.status_code)
-                imgtype = imgURL.split('.')[-1]
-                if imgtype.find('/'):
-                    imgtype = 'jpg'
-                open((localPath + '/%d.' + imgtype) % x, 'wb').write(ir.content)
-                
-                x += 1
-            else:
-                print('\n图片链接不存在%s' % str(x))
-    print(u'下载结束,成功 %s 个,失败 %s 个' % (x-error_num-1, error_num))
+                img_number += 1              
+
+    print(u'\n下载结束,成功 %s 个,失败 %s 个' % (img_number-1, url_number-img_number+1))    
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--keywords_list', help='a text file include keywords.')
+    parser.add_argument('--output_dir', help='Directory with download images')
+    return parser.parse_args(argv)
 
 if __name__ == '__main__':
-    
-    keywords = u'人脸'
-    localPath = '/data0/users/wangtengfei/pictures/'
-    pages = 100
-    
-    dataList = getManyPages(keywords, pages)  # 参数1:关键字，参数2:要下载的页数
-    getImg(dataList, localPath) # 参数2:指定保存的路径
+    args = parse_arguments(sys.argv[1:])
+    keywords_path = args.keywords_list
+    local_path = args.output_dir
+    keywordslist = getKeyword(keywords_path)
+    for keyword in  keywordslist:
+        main(keyword, local_path)
